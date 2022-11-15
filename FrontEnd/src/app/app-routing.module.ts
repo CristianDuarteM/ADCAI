8c1e99ab3be80c7f322da8e7936026f8b98b1cc1
Karen Beltran <karenbrigidbv@ufps.edu.co>
2022-11-14 19:10:07 -0500

    Ajustes en contenido del front

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
 import { HomeComponent } from "./home/home.component";
-import { NavbarComponent } from "./navbar/navbar.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent, pathMatch: "full" },
   { path: "login", component: LoginComponent, pathMatch: "full" },
-  { path: "prueba", component: NavbarComponent, pathMatch: "full" }
 ];
 
 @NgModule({
