36da1c41f204a1d2b19d3ae42747495f0fdbab50
Karen Beltran <karenbrigidbv@ufps.edu.co>
2022-12-04 17:26:19 -0500

    Maquetación para agregar un docente manual

--- FrontEnd/src/app/pages/teacher/add-manual-teacher/add-manual-teacher.component.ts
+++ FrontEnd/src/app/pages/teacher/add-manual-teacher/add-manual-teacher.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgxPermissionsService } from 'ngx-permissions';
 
 @Component({
   selector: 'app-add-manual-teacher',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddManualTeacherComponent implements OnInit {
 
-  constructor() { }
+  backRouteTeacher: string;
+  titleTeacher: string;
+  isPrincipalTeacher: boolean;
+  teacher: FormGroup;
+
+  constructor(private ngxPermissonsService: NgxPermissionsService, private navigation: Router) {
+    this.backRouteTeacher = '/gestion-docentes/agregar';
+    this.titleTeacher = 'Agregar Docentes - Manual';
+    this.isPrincipalTeacher = false;
+    this.teacher = new FormGroup({
+      selectedFaculty: new FormControl({value: 'Facultad seleccionada', disabled: true}),
+      selectedDepartment: new FormControl({value: 'Departamento seleccionado', disabled: true}),
+      email: new FormControl('')
+    });
+  }
 
   ngOnInit(): void {
+    let activeRole = sessionStorage.getItem("activeRole") || '';
+    this.ngxPermissonsService.loadPermissions([activeRole]);
+  }
+
+  addManualTeacher() {
+    this.navigation.navigate(['/gestion-docentes']);
   }
 
 }
