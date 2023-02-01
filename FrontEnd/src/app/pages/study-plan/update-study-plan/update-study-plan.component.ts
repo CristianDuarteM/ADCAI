3b147767bd8e541f0a25751a478456969491fbce
Karen Beltran <karenbrigidbv@ufps.edu.co>
2023-01-31 21:05:19 -0500

    Implementacion para ver y actualizar un plan de estudios

--- FrontEnd/src/app/pages/study-plan/update-study-plan/update-study-plan.component.ts
+++ FrontEnd/src/app/pages/study-plan/update-study-plan/update-study-plan.component.ts
@@ -1,9 +1,12 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { config } from 'src/app/constants/config';
 import { Dialog } from 'src/app/models/Dialog';
+import { StudyPlanRequest } from 'src/app/models/request/StudyPlanRequest';
 import { FacultyResponse } from 'src/app/models/response/FacultyResponse';
+import { StudyPlanResponse } from 'src/app/models/response/StudyPlanResponse';
 import { RolePermission } from 'src/app/models/RolePermission';
 import { FacultyService } from 'src/app/services/faculty/faculty.service';
 import { StudyPlanService } from 'src/app/services/studyPlan/study-plan.service';
@@ -20,6 +23,8 @@ export class UpdateStudyPlanComponent implements OnInit {
   isPrincipal: boolean;
   studyPlanForm: FormGroup;
   facultyList: FacultyResponse[];
+  idStudyPlan: string;
+  studyPlan: StudyPlanResponse;
   isLoaded: boolean;
 
   constructor(private rolePermission: RolePermission, private facultyService: FacultyService, private dialog: Dialog,
@@ -29,30 +34,52 @@ export class UpdateStudyPlanComponent implements OnInit {
     this.isPrincipal = false;
     this.studyPlanForm = new FormGroup({});
     this.facultyList = [];
+    this.idStudyPlan = this.route.snapshot.paramMap.get('idStudyPlan') || '';
+    this.studyPlan = new StudyPlanResponse();
     this.isLoaded = false;
   }
 
   ngOnInit(): void {
     this.rolePermission.loadRole();
     this.getFacultyList();
-    //this.loadStudyPlanData();
 
+    let activeRoleFaculty = (sessionStorage.getItem(config.SESSION_STORAGE.ACTIVE_ROLE) === 'DIRECTOR') ? true : false;
     this.studyPlanForm = new FormGroup({
-      facultyInput: new FormControl(''),
-      nameInput: new FormControl(''),
-      state: new FormControl(''),
+      facultyInput: new FormControl({value: '', disabled: activeRoleFaculty}, Validators.required),
+      nameInput: new FormControl('', Validators.required),
+      state: new FormControl('', Validators.required),
     });
   }
 
   onSubmit() {
+    if(this.studyPlanForm.valid) {
+      let studyPlanData: StudyPlanRequest = {
+        nombre: this.studyPlanForm.get('nameInput')?.value,
+        id_facultad: this.studyPlanForm.get('facultyInput')?.value,
+        estado: (this.studyPlanForm.get('state')?.value === 'true') ? true : false,
+      };
 
+      if(this.studyPlan.nombre === studyPlanData.nombre) {
+        delete studyPlanData.nombre;
+      }
+      this.studyPlanService.updateStudyPlan(this.idStudyPlan, studyPlanData).subscribe({
+        next: studyPlanResponse => {
+          this.dialog.openDialog(studyPlanResponse.msg, '/gestion-plan-estudio');
+        },
+        error: (error: HttpErrorResponse) => {
+          this.dialog.openDialog(this.dialog.getErrorMessage(error), '/login');
+        }
+      });
+    } else {
+      this.dialog.openDialog('¡Diligencie los campos faltantes!', '');
+    }
   }
 
   getFacultyList() {
     this.facultyService.getFacultyList().subscribe({
       next: facultyListResponse => {
         this.facultyList = facultyListResponse.rows;
-        this.isLoaded = true;
+        this.loadStudyPlanData();
       },
       error: (error: HttpErrorResponse) => {
         this.dialog.openDialog(this.dialog.getErrorMessage(error), '/login');
@@ -61,10 +88,15 @@ export class UpdateStudyPlanComponent implements OnInit {
   }
 
   loadStudyPlanData() {
-    let idStudyPlan = this.route.snapshot.paramMap.get('idStudyPlan') || '';
-    this.studyPlanService.getStudyPlanById(idStudyPlan).subscribe({
+    this.studyPlanService.getStudyPlanById(this.idStudyPlan).subscribe({
       next: studyPlanResponse => {
-        console.log(studyPlanResponse);
+        this.studyPlan = studyPlanResponse;
+        this.studyPlanForm.setValue({
+          facultyInput: studyPlanResponse.facultad.id,
+          nameInput: studyPlanResponse.nombre,
+          state: (studyPlanResponse.estado) ? 'true' : 'false',
+        });
+        this.isLoaded = true;
       },
       error: (error: HttpErrorResponse) => {
         this.dialog.openDialog(this.dialog.getErrorMessage(error), '/login');
--- FrontEnd/src/app/services/studyPlan/study-plan.service.ts
+++ FrontEnd/src/app/services/studyPlan/study-plan.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { config } from 'src/app/constants/config';
+import { StudyPlanRequest } from 'src/app/models/request/StudyPlanRequest';
 
 @Injectable({
   providedIn: 'root'
@@ -44,6 +45,14 @@ export class StudyPlanService {
       nombres: nameList,
     };
     return this.httpClient.post(config.API_URL + '/api/plan_estudios', addStudyPlanBody, {
+      headers: {
+        'x-token': this.tokenSession
+      }
+    });
+  }
+
+  updateStudyPlan(idStudyPlan: string, studyPlanBody: StudyPlanRequest): Observable<any> {
+    return this.httpClient.put(config.API_URL + '/api/plan_estudios/' + idStudyPlan, studyPlanBody, {
       headers: {
         'x-token': this.tokenSession
       }
