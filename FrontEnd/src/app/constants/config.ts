b4250a77f4987af856bfbfd9125a7cc997a2133b
Cristian Duarte <43598199+CristianDuarteM@users.noreply.github.com>
2023-02-03 10:18:41 -0500

    Docker compose con los 3  servicios bd,back,front con usuario admin=ufpsadcai

--- FrontEnd/src/app/constants/config.ts
+++ FrontEnd/src/app/constants/config.ts
@@ -1,5 +1,6 @@
 export class config {
-  public static API_URL = 'https://adcai-backend-production.up.railway.app';
+  //public static API_URL = 'https://adcai-backend-production.up.railway.app';
+  public static API_URL = 'http://localhost:8080';
   public static CLIENT_ID_GOOGLE = '742126564330-sia6o8jmu6jni7ocpg68n5n3iagp12ni.apps.googleusercontent.com';
   public static SESSION_STORAGE = {
     TOKEN_GOOGLE: 'tokenGoogle',
